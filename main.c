d58c729bfc7691fe0db856dc7c5e2ebfb870c3e4
Maulaz Tony <tony.maulaz@heig-vd.ch>
2022-04-27 18:47:51 +0200

    In progress

--- main.c
+++ main.c
@@ -1,1 +1,32 @@
-int main(){}
+#include "stdio.h"
+#include "stdlib.h"
+#include <stdint.h>
+
+#pragma pack(1)
+typedef struct {
+    char mthd[4];
+    //char c1;
+    //char c2;
+    //char c3;
+    //char c4;
+    uint16_t header_size;
+    uint16_t header_size1;
+    uint16_t format;
+    uint16_t nbr_track;
+    uint16_t division;
+}Header;
+
+int main(){
+    FILE* fp = fopen("test.mid", "r");
+    if( fp == NULL )
+        exit(1);
+
+    char buff_in[100] = {0};
+    Header h;
+    fread(&h, sizeof(Header), 1, fp);
+    //fread(&h, 1, sizeof(Header), fp);
+    //fread(buff_in, 8, 1, fp);
+    //fread(buff_in, 4, 1, fp);
+
+    fclose(fp);
+}
